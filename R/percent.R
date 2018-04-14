dd70f511c92bbe4b342b23046fdec4fecb4fcf31
kang-yu <kang.yu@live.com>
2018-04-14 22:31:34 +0200

    update percent

--- R/percent.R
+++ R/percent.R
@@ -1,13 +1,13 @@
 #' Format numbers to percent
 #'
-#' Formats numbers to percentage.
+#' Formats a numeric vector to percentage string vector.
 #'
 #' @param x A numeric vector.
-#' @param digits A integer to define the number of dicimals.
+#' @param digits A integer to define the number of dicimals, default is 2.
 #' @param \dots Arguments passed to \code{\link{formatC}}.
 #' @seealso \code{\link{formatC}} and \code{\link{sprintf}}
 #' @examples
-#' percent(1:3)
+#' percent(1:3, 1)
 
 percent <- function(x, digits = 2, format = "f", ...) {
   paste0(formatC(100 * x, format = format, digits = digits, ...), "%")
