3d36a01bf01b7dfdd84bd96b2ff9284fecbdc28f
kang-yu <kang.yu@live.com>
2018-04-14 23:00:23 +0200

    add function use_color

---
+++ R/use_colour.R
@@ -0,0 +1,13 @@
+#' Use the ggplot style colors
+#'
+#' Create a vector of colors of the ggplot style.
+#'
+#' @param n The number of colors to be generated.
+#' @seealso \code{\link{hcl}}
+#' @example
+#' ggplotColours(3)
+
+ggplotColours <- function(n = 6, h = c(0, 360) + 15){
+  if ((diff(h) %% 360) < 1) h[2] <- h[2] - 360/n
+  hcl(h = (seq(h[1], h[2], length = n)), c = 100, l = 65)
+}
