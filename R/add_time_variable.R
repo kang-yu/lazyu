3223a790f052e819dd7773e0dda6dbbf9d7547c8
kang-yu <kang.yu@live.com>
2018-11-23 22:31:24 +0100

    minor changes in description and readme

--- R/add_time_variable.R
+++ R/add_time_variable.R
@@ -7,7 +7,6 @@
 #'   \item{df}{Returns a df containing the created variables: month, month.abb, monthday, week, weekday, etc.}
 #'
 #' @export
-#'
 
 add_variable_monthweek <- function(df, datevar = "date", ...){
 
