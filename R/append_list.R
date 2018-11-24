7243af9c2f5cc45f6ec7994a87f7cd715617b3e3
kang-yu <kang.yu@live.com>
2018-11-24 14:47:57 +0100

    changed append_list

--- R/append_list.R
+++ R/append_list.R
@@ -1,6 +1,6 @@
-#' Append r object to a list
+#' Append R object to a list.
 #'
-#' @param list R list.
+#' @param l A R-list.
 #' @param obj R object to be appended to the list.
 #' @param obj.name R object name to be used to the list.
 #' @param overwrite logical value indicates whether overwrite object with the same name.
@@ -8,18 +8,19 @@
 #'   \item{list_new}
 #' @seealso \code{\link{hcl}}
 #' @examples
-#' list <- list(c("a", "b"))
+#' l <- list(c("a", "b"))
 #' x <- list(c(1:5))
-#' append_list(list, x, "X")
+#' append_list(l, x, "X")
 #' @export
 
-append_list <- function(list, obj, obj.name, overwrite=TRUE){
+append_list <- function(l, obj, obj.name, overwrite = TRUE){
+
   if (!is.list(obj)) obj <- list(obj)
-  if (!is.null(list[[obj.name]]) & overwrite == TRUE){
-    list_new <- list
-    list_new[obj.name] <- obj
+  if (!is.null(l[[obj.name]]) & overwrite==TRUE){
+    l_new <- l
+    l_new[obj.name] <- obj
   } else {
-    list_new <- c(list,obj)
-    names(list_new)[length(list_new)] <- obj.name
+    l_new <- c(l, obj)
+    names(l_new)[length(l_new)] <- obj.name
   }
 }
